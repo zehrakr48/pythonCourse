001094e0326d592f555ce5653f9b4ba8c8518495
Zehra KIR <zzehrakr48@gmail.com>
2024-10-23 13:54:31 +0300

    Def
    Creating a Function

---
+++ def.py
@@ -0,0 +1,13 @@
+def studentsFunction():
+    students = ["fatma", "ahmet", "zehra", "ali", "ayfer", "enes", "bugra", "elisa"]
+
+    # for each student in the students list
+    for student in students:
+        print(student)  # print the student's name
+        print(" ")  # print a blank space
+
+
+# Call the function multiple times
+studentsFunction()
+studentsFunction()
+studentsFunction()
