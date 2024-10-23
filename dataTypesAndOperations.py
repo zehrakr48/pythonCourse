4225a8eb7de611c4a37e8e1c0f0cd47d0d270c6b
Zehra KIR <zzehrakr48@gmail.com>
2024-10-23 12:53:11 +0300

    Data Types and Operations
    Data Type Demonstration and Simple Arithmetic Operations

---
+++ dataTypesAndOperations.py
@@ -0,0 +1,49 @@
+# String variable
+x = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
+# Integer variable
+y = 794
+
+# Float variable
+z = 60.75
+
+# Large integer variable
+b = 211200060
+
+# String variable
+a = "a"
+
+# Negative float variable
+c = -12345678.90
+
+# String that can be converted to a number
+d = "4427"
+
+# Concatenating and printing the string 'x'
+print("x = ", x)
+
+# Printing the integer, float, and large integer
+print("y = ", y)
+print("z = ", z)
+print("b = ", b)
+
+# Printing the string variable 'a'
+print("a = ", a)
+
+# Printing the negative float value 'c'
+print("c = ", c)
+
+# String merging
+print("d + '38' = ", d + "38")
+# Converting string 'd' to float, adding 38, and printing the result
+print("float(d) + 38 = ", float(d) + 38)
+
+
+# Printing the data types of each variable
+print("type of x ", type(x))
+print("type of y ", type(y))
+print("type of z ", type(z))
+print("type of b ", type(b))
+print("type of a ", type(a))
+print("type of c ", type(c))
+print("type of d ", type(d))
