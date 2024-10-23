87b1301f657e2e7c94c15815628ebe5182a3c441
Zehra KIR <zzehrakr48@gmail.com>
2024-10-23 13:02:09 +0300

    If Elif Else
    Conditional Comparisons Using If-Elif-Else

---
+++ ifElifElse.py
@@ -0,0 +1,29 @@
+# Assigning values to variables
+a = 6236054
+b = 2112060
+c = 7384427
+d = 6236054
+
+# Comparing a and b
+if a < b:  # Check if a is less than b
+    print("a is smaller than b.")  # Print if a is smaller than b
+elif a > b:  # Check if a is greater than b
+    print("a is greater than b.")  # Print if a is greater than b
+else:  # If a is neither greater nor smaller, it must be equal to b
+    print("a is equal to b.")  # Print if a is equal to b
+
+# Comparing a and c
+if a < c:  # Check if a is less than c
+    print("a is smaller than c.")  # Print if a is smaller than c
+elif a > c:  # Check if a is greater than c
+    print("a is greater than c.")  # Print if a is greater than c
+else:  # If a is neither greater nor smaller, it must be equal to c
+    print("a is equal to c.")  # Print if a is equal to c
+
+# Comparing a and d
+if a < d:  # Check if a is less than d
+    print("a is smaller than d.")  # Print if a is smaller than d
+elif a > d:  # Check if a is greater than d
+    print("a is greater than d.")  # Print if a is greater than d
+else:  # If a is neither greater nor smaller, it must be equal to d
+    print("a is equal to d.")  # Print if a is equal to d
