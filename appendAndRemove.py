184dff0d6e78e57ce9e2a58f2522884a5bf48286
Zehra KIR <zzehrakr48@gmail.com>
2024-10-28 17:28:08 +0300

    Append and Remove Functions
    Manipulating a List

---
+++ appendAndRemove.py
@@ -0,0 +1,19 @@
+# Define the list of students
+students = ["Fatma", "Ahmet", "Zehra"]
+
+# Print the first element in the list
+print(students[0])
+
+# Add elements to the list
+students.append("Kazım")
+students.append("Şevval")
+print(students)
+
+# Remove elements from the list
+students.remove("Kazım")
+students.remove("Şevval")
+print(students)
+
+# Modify an element in the list
+students[2] = "Elif"
+print(students)
