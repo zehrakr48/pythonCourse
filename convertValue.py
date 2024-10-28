1e356072d783abc1abd7e8765239e739ac9b33b9
Zehra KIR <zzehrakr48@gmail.com>
2024-10-28 17:26:12 +0300

    Convert Value
    Convert Kilometers to Miles

---
+++ convertValue.py
@@ -0,0 +1,10 @@
+# Get the kilometer value from the user
+km = float(
+    input("Please enter the kilometer value: ")
+)  # Prompt the user for the kilometer value and convert it to float
+
+# Convert kilometers to miles
+mile = km / 1.609344
+
+# Print the mile value
+print("Mile value:", mile)
