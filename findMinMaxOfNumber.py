c59b175cd5585f3015566370899c7f3dd153607b
Zehra KIR <zzehrakr48@gmail.com>
2024-10-28 12:35:27 +0300

    Find Min and Max of Numbers
    Finding the Smallest and Largest Number Among Three Variables

---
+++ findMinMaxOfNumber.py
@@ -0,0 +1,40 @@
+# Define the variables a, b, and c with their respective values
+a = 6235460
+b = 2116020
+c = 7382744
+
+# Check if 'a' is less than 'b'
+if a < b:
+    # Check if 'a' is also less than 'c'
+    if a < c:
+        print("The smallest number is a.")  # 'a' is the smallest number
+
+# Check if 'b' is less than 'a'
+if b < a:
+    # Check if 'b' is also less than 'c'
+    if b < c:
+        print("The smallest number is b.")  # 'b' is the smallest number
+
+# Check if 'c' is less than 'b'
+if c < b:
+    # Check if 'c' is also less than 'a'
+    if c < a:
+        print("The smallest number is c.")  # 'c' is the smallest number
+
+# Check if 'a' is greater than 'b'
+if a > b:
+    # Check if 'a' is also greater than 'c'
+    if a > c:
+        print("The largest number is a.")  # 'a' is the largest number
+
+# Check if 'b' is greater than 'a'
+if b > a:
+    # Check if 'b' is also greater than 'c'
+    if b > c:
+        print("The largest number is b.")  # 'b' is the largest number
+
+# Check if 'c' is greater than 'b'
+if c > b:
+    # Check if 'c' is also greater than 'a'
+    if c > a:
+        print("The largest number is c.")  # 'c' is the largest number
