6f1136e9a65dbfafdf8b80f13c89d1862d68f065
Zehra KIR <zzehrakr48@gmail.com>
2024-10-28 13:13:41 +0300

    Substring
    String Slicing, Extracting Substrings

---
+++ substring.py
@@ -0,0 +1,13 @@
+# Define the message
+message = "python is awesome!"
+
+# Print the characters from index 2 to 6 (inclusive of 2, exclusive of 6)
+print(message[2:6])
+
+# Print everything after index 10 (inclusive of 10)
+print(message[10:])
+
+# Print everything up to index 6 (exclusive of 6)
+print(message[:6])
+
+# In Python, character indexing starts at 0
