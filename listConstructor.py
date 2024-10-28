0d2c92480b89c1eedecfbcbff2c2fd1bd728a922
Zehra KIR <zzehrakr48@gmail.com>
2024-10-28 17:30:06 +0300

    List Constructor
     Creating and Working with a List

---
+++ listConstructor.py
@@ -0,0 +1,8 @@
+# Create a list of cities using the list constructor
+cities = list(("Gaziantep", "Sakarya"))
+
+# Print the list of cities
+print(cities)
+
+# Print the length of the cities list
+print(len(cities))
