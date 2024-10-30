344d7dfef07e9905d4774023a08010b06ef01adc
Zehra KIR <zzehrakr48@gmail.com>
2024-10-30 17:12:17 +0300

    Sort Function
    Sorting Lists: the Sort Method to Order Elements Alphabetically

---
+++ sortFunction.py
@@ -0,0 +1,8 @@
+# Create a list
+cars1 = list(("Volvo", "Mercedes", "Skoda", "Nissan", "Bmw"))
+# Print the contents of cars1
+print("Cars1 list: ", cars1)
+# Sort the elements in cars1 in ascending alphabetical order
+cars1.sort()
+# Print the contents of cars1
+print("Cars1 list: ", cars1)
