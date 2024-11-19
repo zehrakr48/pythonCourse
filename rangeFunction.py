0afa4029f7d06a3d9b973d7e07367625e6b27774
Zehra KIR <zzehrakr48@gmail.com>
2024-11-19 13:03:38 +0300

    Range Function
    Iterating over numbers via using Range Function

---
+++ rangeFunction.py
@@ -0,0 +1,11 @@
+print("Numbers from 1 to 10:")
+for x in range(10):
+    print(x + 1)
+
+print("Numbers from 5 to 10:")
+for x in range(5, 11):
+    print(x)
+
+print("Even numbers from 1 to 10:")
+for x in range(2, 11, 2):
+    print(x)
