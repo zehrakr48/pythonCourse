f14724b50b0fa9552dabafc95ae87c711bd2facf
Zehra KIR <zzehrakr48@gmail.com>
2024-11-19 12:58:18 +0300

    Continue
    Testing the usage of "Continue" keyword in a For Loop

---
+++ continue.py
@@ -0,0 +1,18 @@
+colours = [
+    "Yellow",
+    "Orange",
+    "Red",
+    "Pink",
+    "Purple",
+    "Blue",
+    "Navy Blue",
+    "Green",
+    "Khaki",
+    "Brown",
+    "Gray",
+]
+
+for colour in colours:
+    if colour == "Purple":
+        continue
+    print(colour)
