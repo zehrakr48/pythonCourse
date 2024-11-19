d9b2d8ef49ed293f4045288a3bbf77927588c223
Zehra KIR <zzehrakr48@gmail.com>
2024-11-19 12:56:12 +0300

    Break
    Testing the usage of "Break" keyword in a For Loop

---
+++ break.py
@@ -0,0 +1,18 @@
+colours = [
+    "Yellow",
+    "Orange",
+    "Red",
+    "Pink",
+    "Purple",
+    "Blue",
+    "Navy Blue",
+    "Green",
+    "Khaki",
+    "Brown",
+    "Gray",
+]
+
+for colour in colours:
+    print(colour)
+    if colour == "Purple":
+        break
