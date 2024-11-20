317684119eb00051d3969b54d6355ecaa3bcf702
Zehra KIR <zzehrakr48@gmail.com>
2024-11-20 11:08:54 +0300

    Create a Custom Module
    Create a custom module to perform four operations

---
+++ library/library.py
@@ -0,0 +1,7 @@
+# Imports the module named "module" and gives it the alias "md".
+import module as md
+
+md.add(48, 54)
+md.subtract(34, 27)
+md.multiply(7, 14)
+md.divide(120, 8)
---
+++ library/module.py
@@ -0,0 +1,14 @@
+def add(number1, number2):
+    print("Sum of the numbers:", number1 + number2)
+
+
+def subtract(number1, number2):
+    print("Difference of the numbers:", number1 - number2)
+
+
+def multiply(number1, number2):
+    print("Product of the numbers:", number1 * number2)
+
+
+def divide(number1, number2):
+    print("Division of the numbers:", number1 / number2)
